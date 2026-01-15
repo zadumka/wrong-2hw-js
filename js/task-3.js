408fe9238fc5b624765278b1ee2ccd5a0960fec4
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-15 12:25:48 +0200

    Refactor getElementWidth and remove checkForSpam
    Refactor getElementWidth function for clarity and accuracy. Removed checkForSpam function and added console logs for getElementWidth.

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,12 @@
 'use strict';
-
 function getElementWidth(content, padding, border) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
-  
+  const paddingValue = parseFloat(padding);
+  const borderValue = parseFloat(border);
 
-  return contentWidth + 2 * (paddingWidth + borderWidth);
+  const totalWidth = contentValue + paddingValue + borderValue;
 }
+return `${totalWidth}px`;
 
-function checkForSpam(message) {
-  if (
-    message.toLowerCase().includes('spam') ||
-    message.toLowerCase().includes('sale')
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
