65c20b94271342ed0157fa65c6f75f5bf8e81edb
zadumka <90329284+zadumka@users.noreply.github.com>
2026-01-15 12:10:57 +0200

    Refactor shipping message and remove formatMessage
    Removed the formatMessage function and its console logs. Updated getShippingMessage function to correctly format the shipping message.

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,11 @@
 'use strict';
-const contentWidth = 0;
-let paddingWidth;
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return message;
-  } else {
-    return `${message.slice(0, maxLength)}...`;
-  }
+
+
+function getShippingMessage(country, price, deliveryFee) {
+const totalPrice = price;
+return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
-console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
-console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
-console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
-console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage(80, "Germany", 20));
+console.log(getShippingMessage("Sweden", 100, 20));
